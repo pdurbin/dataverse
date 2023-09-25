927caba57828e1c13309742006be54778dd583ef
Philip Durbin <philip_durbin@harvard.edu>
2023-09-25 12:44:39 -0400

    scratch

--- src/main/java/edu/harvard/iq/dataverse/DatasetPage.java
+++ src/main/java/edu/harvard/iq/dataverse/DatasetPage.java
@@ -392,6 +392,8 @@ public class DatasetPage implements java.io.Serializable {
     List<ExternalTool> fileQueryTools = new ArrayList<>();
     private List<ExternalTool> datasetExploreTools;
     private List<ExternalTool> datasetConfigureTools;
+    // The selected dataset-level configure tool
+    private ExternalTool datasetConfigureTool;
 
     public Boolean isHasRsyncScript() {
         return hasRsyncScript;
@@ -5576,6 +5578,16 @@ public class DatasetPage implements java.io.Serializable {
 
     public List<ExternalTool> getDatasetConfigureTools() {
         return datasetConfigureTools;
+    }
+
+    public ExternalTool getDatasetConfigureTool() {
+        System.out.println("calling getDatasetConfigureTool...");
+        return datasetConfigureTool;
+    }
+
+    public void setDatasetConfigureTool(ExternalTool datasetConfigureTool) {
+        System.out.println("calling setDatasetConfigureTool using " + datasetConfigureTool);
+        this.datasetConfigureTool = datasetConfigureTool;
     }
 
     Boolean thisLatestReleasedVersion = null;
