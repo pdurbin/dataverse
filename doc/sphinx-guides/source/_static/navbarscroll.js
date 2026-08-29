064b11232c53e6a44f463e0a177f5082c8e4cfc5
Philip Durbin <philip_durbin@harvard.edu>
2026-08-28 21:45:42 -0400

    handle anchors better
    Previously, anchors didn't work well on pages with images.
    
    You'd land on the wrong part of the page.

--- doc/sphinx-guides/source/_static/navbarscroll.js
+++ doc/sphinx-guides/source/_static/navbarscroll.js
@@ -1,29 +1,74 @@
-/*  
-    Use to fix hidden section headers behind the navbar when using links with targets
-    See: https://stackoverflow.com/questions/10732690/offsetting-an-html-anchor-to-adjust-for-fixed-header    
+/*
+    Use to fix hidden section headers behind the navbar when using links with targets.
+    Re-apply on full page load to account for late layout shifts (e.g. images).
 */
-$jqTheme(document).ready(function() {
-  $jqTheme('a[href*="#"]:not([href="#"])').on('click', function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
-&& location.hostname == this.hostname) {
-      var target = $jqTheme(this.hash);
-      target = target.length ? target : $jqTheme('a.headerlink[href="#' + this.hash.slice(1) +'""]');
-      if (target.length) {
-        $jqTheme('html,body').animate({
-          scrollTop: target.offset().top - 60 //offsets for fixed header
-        }, 1000);
-        return false;
-      }
+(function($) {
+  var headerOffset = 60;
+
+  function getTarget(hashValue) {
+    if (!hashValue || hashValue === '#') {
+      return $();
     }
+
+    var hash = hashValue.charAt(0) === '#' ? hashValue : '#' + hashValue;
+    var decodedHash = hash;
+
+    try {
+      decodedHash = '#' + decodeURIComponent(hash.slice(1));
+    } catch (ignore) {
+    }
+
+    var target = $(decodedHash);
+    if (!target.length && decodedHash !== hash) {
+      target = $(hash);
+    }
+    if (!target.length) {
+      target = $('a.headerlink[href="' + decodedHash.replace(/"/g, '\\"') + '"]');
+    }
+
+    return target.first();
+  }
+
+  function scrollToHash(hashValue, animate) {
+    var target = getTarget(hashValue || window.location.hash);
+
+    if (!target.length) {
+      return false;
+    }
+
+    var scrollTop = Math.max(target.offset().top - headerOffset, 0);
+
+    if (animate) {
+      $('html,body').stop(true).animate({ scrollTop: scrollTop }, 200);
+    } else {
+      window.scrollTo(0, scrollTop);
+    }
+
+    return true;
+  }
+
+  $(document).ready(function() {
+    $('a[href*="#"]:not([href="#"])').on('click', function(event) {
+      var samePath = location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '');
+      var sameHost = location.hostname === this.hostname;
+
+      if (samePath && sameHost && scrollToHash(this.hash, true)) {
+        event.preventDefault();
+      }
+    });
+
+    if (window.location.hash) {
+      scrollToHash(window.location.hash, false);
+    }
+
+    $(window).on('hashchange', function() {
+      scrollToHash(window.location.hash, false);
+    });
+
+    $(window).on('load', function() {
+      if (window.location.hash) {
+        scrollToHash(window.location.hash, false);
+      }
+    });
   });
-  //Executed on page load with URL containing an anchor tag.
-  if($jqTheme(location.href.split("#")[1])) {
-      var target = $jqTheme('#'+location.href.split("#")[1]);
-      if (target.length) {
-        $jqTheme('html,body').animate({
-          scrollTop: target.offset().top - 60 //offset height of header here too.
-        }, 1000);
-        return false;
-      }
-    }
-});
+})($jqTheme);
